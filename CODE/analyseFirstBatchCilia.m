e87a6d28c96b3f85b9d976a6f1b62f8747b6463f
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2025-07-18 16:54:31 +0100

    Update analyseFirstBatchCilia.m

--- CODE/analyseFirstBatchCilia.m
+++ CODE/analyseFirstBatchCilia.m
@@ -1,13 +1,34 @@
 
 
 baseDir     = 'C:\Users\sbbk034\OneDrive - City, University of London\Acad\Research\SGUL_Cilia\TIFFS_2025_07_11';
-dir0        = dir(strcat(baseDir,filesep,'*.tif'));
+dir0        = dir(strcat(baseDir,filesep,'*OVER.tif'));
 
 numFiles    = numel(dir0);
 
 
+%% read all files and display maximum intensity per channel
+for k=1:numFiles
+    disp(k)
+    shortName{k}            = dir0(k).name(26:34);
+    currFile                = strcat(baseDir,filesep,dir0(k).name);
+    CiliaVolume             = readCilia(currFile);
+    maxIntensities(k,:)     = max(max(max((CiliaVolume))),[],4);
+    meanIntensities(k,:)    = median(median(median((CiliaVolume))),4);
+end
 
+h0=figure;
+h1=gca;
+hold off
+plot(1:numFiles,maxIntensities(:,1),'r-o',1:numFiles,maxIntensities(:,2),'g-o',1:numFiles,maxIntensities(:,3),'b-o')
+grid on
 
+title('Maximum Intensities per Channel')
+h1.TickLabelInterpreter = 'none';
+h1.XTick                = 1:numFiles;
+h1.XTickLabel           = shortName;
+h1.XTickLabelRotation   = 90;
+h1.Position             = [  0.0674    0.27    0.9112    0.60];
+h0.Position             = [  643.4000   97.8000  812.0000  333.6000];
 %%
 for k=87%:numFiles
 
@@ -37,7 +58,7 @@ for k=91%:numFiles
 
     currFile    = strcat(baseDir,filesep,dir0(k).name);
 
-    
+
     b           = imfinfo(currFile);
 
     clear a
