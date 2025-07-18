1ce6a40d167bf78951da1d6f5b8027516a7b15b3
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2025-07-18 16:54:34 +0100

    Update readCilia.m

--- CODE/readCilia.m
+++ CODE/readCilia.m
@@ -1,14 +1,12 @@
 function CiliaVolume = readCilia(currFile)
 
+b                                       = imfinfo(currFile);
+numSlices                               = numel(b);
 
-    
-    b           = imfinfo(currFile);
-    numSlices   = numel(b);
-
-    CiliaVolume(b.Width,b.Height,numSlices) =0;
-    for k2=1:3:numel(b)
-        %tempImage           = double(imread(currFile,k2));
-        CiliaVolume(:,:,1,1+((k2)-1)/3)         = double(imread(currFile,k2+2));  
-        CiliaVolume(:,:,2,1+((k2)-1)/3)         = double(imread(currFile,k2+1));  
-        CiliaVolume(:,:,3,1+((k2)-1)/3)         = double(imread(currFile,k2+0));  
-    end
+CiliaVolume(b(1).Width,b(1).Height,3,numSlices/3) = 0;
+for k2=1:3:numel(b)
+    %tempImage           = double(imread(currFile,k2));
+    CiliaVolume(:,:,1,1+((k2)-1)/3)     = double(imread(currFile,k2+2));
+    CiliaVolume(:,:,2,1+((k2)-1)/3)     = double(imread(currFile,k2+1));
+    CiliaVolume(:,:,3,1+((k2)-1)/3)     = double(imread(currFile,k2+0));
+end
