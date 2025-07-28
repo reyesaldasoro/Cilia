e585ad1168b62778a631ca41817c89b21118abdd
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2025-07-28 16:49:10 +0100

    segment cilia and dapi

--- CODE/segmentCilia.m
+++ CODE/segmentCilia.m
@@ -1,15 +1,29 @@
 function CiliaSegmented = segmentCilia(CiliaVolume)
 
+%% Separate the channels
 
+DAPI                    = squeeze(CiliaVolume(:,:,3,:));
+Green                   = squeeze(CiliaVolume(:,:,2,:));
+
+
+[rows,cols,numSlices]   = size(DAPI);
+
+%%
 % Perform a hysteresis thresholding for the cilia
-[q1,q2] = bwlabeln(squeeze(CiliaVolume(:,:,2,:))>900);
-[q3,q4] = bwlabeln(squeeze(CiliaVolume(:,:,2,:))>300);
-
-[CiliaSegmented,q6] = bwlabeln(ismember(q3,unique(q3(q1>0))));
+[q1,q2]                 = bwlabeln(Green>900);
+[q3,q4]                 = bwlabeln(Green>300);
+q5                      = ismember(q3,unique(q3(q1>0)));
+[CiliaSegmented,q6]     = bwlabeln(ismember(q3,unique(q3(q1>0))));
 q7=regionprops(q5,squeeze(CiliaVolume(:,:,2,:)),'area',"MeanIntensity","MaxIntensity","MinIntensity");
+%%
 
-% Segment DAPI based on intensity
+%% Segment DAPI based on CellPose
+cp                      = cellpose(Model="nuclei");
 
+%NucleiSegmented(:,:,k)          =  segmentCells2D(cp,max(DAPI,[],3),ImageCellDiameter=60);
+for k=1:numSlices
+    NucleiSegmented(:,:,k)          =  segmentCells2D(cp,DAPI(:,:,k),ImageCellDiameter=60);
+end
 
 % Discard cilia that is FAR from DAPI
 
