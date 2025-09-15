49559b4fddde7e75de0d03377c1c30a664363c92
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2025-09-15 17:47:46 +0100

    Update segmentCiliaPeaks.m

--- CODE/segmentCiliaPeaks.m
+++ CODE/segmentCiliaPeaks.m
@@ -1,23 +1,23 @@
-function ciliaPeaks = segmentCiliaPeaks(Green_MIP,prom)
+function greenPeaks = segmentGreenPeaks(Green_MIP,prom)
 
 % Detect the cilia peaks based on findpeaks in 1D,
 
 % First start with filtering the data
 cilia_MIP_LPF                   = imfilter(Green_MIP,fspecial('gaussian',5,3),'replicate');
-ciliaPeaks_0                    = zeros(size(cilia_MIP_LPF));
+greenPeaks_0                    = zeros(size(cilia_MIP_LPF));
 % run over all the rows and detect peaks, use a very high prominence to
 % only capture significant peaks
 for k=1:size(cilia_MIP_LPF,2)
     [~,locPeaks]                = findpeaks(cilia_MIP_LPF(k,:),'MinPeakProminence',prom,'MinPeakDistance',6);
-    ciliaPeaks_0(k,locPeaks)    = 1;
+    greenPeaks_0(k,locPeaks)    = 1;
 end
 % now extend to the regions around those peaks, use Otsu
 thres_background                =  max(cilia_MIP_LPF(:))*graythresh(cilia_MIP_LPF/max(cilia_MIP_LPF(:)));
-ciliaPeaks_1                    = bwlabel(cilia_MIP_LPF>(thres_background));
-ciliaPeaks_1P                   = regionprops(ciliaPeaks_1,cilia_MIP_LPF,'area','MeanIntensity','MaxIntensity');
+greenPeaks_1                    = bwlabel(cilia_MIP_LPF>(thres_background));
+greenPeaks_1P                   = regionprops(greenPeaks_1,cilia_MIP_LPF,'area','MeanIntensity','MaxIntensity');
 %keep regions that touch the peaks
-ciliaToKeep                     = unique(ciliaPeaks_1(ciliaPeaks_0>0));
-ciliaPeaks1                     = ismember(ciliaPeaks_1,ciliaToKeep(2:end));
+ciliaToKeep                     = unique(greenPeaks_1(greenPeaks_0>0));
+greenPeaks1                     = ismember(greenPeaks_1,ciliaToKeep(2:end));
 %discard regions that are too large (blobs)
-ciliaPeaks2                     = ismember(ciliaPeaks_1,find([ciliaPeaks_1P.Area]<( mean([ciliaPeaks_1P.Area])+ 3*std([ciliaPeaks_1P.Area]))));
-ciliaPeaks                      = ciliaPeaks2.*ciliaPeaks1;
+greenPeaks2                     = ismember(greenPeaks_1,find([greenPeaks_1P.Area]<( mean([greenPeaks_1P.Area])+ 3*std([greenPeaks_1P.Area]))));
+greenPeaks                      = greenPeaks2.*greenPeaks1;
