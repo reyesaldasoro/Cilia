969e13ada0e771bb3d078e3d9c7e7c6a1163d31e
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2025-09-15 17:47:53 +0100

    Update segmentCilia.m

--- CODE/segmentCilia.m
+++ CODE/segmentCilia.m
@@ -79,72 +79,95 @@ end
  %     NucleiSegmented3(:,:,k)          =  segmentCells2D(cp,DAPI(:,:,k),ImageCellDiameter=90,CellThreshold=0,FlowErrorThreshold=10);
  % end
 
-%%
-% Perform a hysteresis thresholding for the cilia
-[q1,q2]                 = bwlabeln(Green>greenT_H);
-[q3,q4]                 = bwlabeln(Green>greenT_L);
-q5                      = ismember(q3,unique(q3(q1>0)));
-[CiliaSegmented,q6]     = bwlabeln(ismember(q3,unique(q3(q1>0))));
-%CiliaSegmented_P        = regionprops(CiliaSegmented,squeeze(Green),'area',"MeanIntensity","MaxIntensity","MinIntensity");
-CiliaSegmented_MIP      = max(CiliaSegmented,[],3);
-%CiliaSegmented_MIP_P    = regionprops(CiliaSegmented_MIP,'Area','Centroid','BoundingBox','MajorAxisLength','Circularity','Orientation','Eccentricity','MinorAxisLength','Orientation');
-
 %% Discard cells that touch boundary
-AllCells                = unique(NucleiSegmented);
-TouchingCells           = union(unique(NucleiSegmented_MIP([1:3 end-3:end],:)),unique(NucleiSegmented_MIP(:,[1:3 end-3:end])));
-NucleiSegmented_NotBorder= ismember(NucleiSegmented_MIP,setdiff(AllCells,TouchingCells));
-NucleiSegmented_Border  = ismember(NucleiSegmented_MIP,(TouchingCells(2:end)));
-
-%% Discard cilia that are FAR from DAPI and are SMALL
-DistFromDAPI            = bwdist(NucleiSegmented_MIP>0);
-DistFromDAPI_notTouching = bwdist(NucleiSegmented_NotBorder);
-
-
-CiliaSegmented_P2       = regionprops(CiliaSegmented,repmat(DistFromDAPI,[1 1 numSlices]),'area',"MeanIntensity","MaxIntensity","MinIntensity");
-CiliaSegmented_P3       = regionprops(CiliaSegmented,repmat(DistFromDAPI_notTouching,[1 1 numSlices]),'area',"MeanIntensity","MaxIntensity","MinIntensity");
-CiliaSegmented_P4       = regionprops(CiliaSegmented_MIP,'area','MajorAxisLength','MinorAxisLength','Eccentricity');
-
-CiliaToKeep1             = find(([CiliaSegmented_P3.MinIntensity]<distFromNucleus)&([CiliaSegmented_P3.Area]>ciliaSize));
-CiliaToKeep2             = find(([CiliaSegmented_P4.MajorAxisLength]./[CiliaSegmented_P4.MinorAxisLength])<10);
-CiliaToKeep             = ismember(CiliaSegmented_MIP,intersect(CiliaToKeep1,CiliaToKeep2));
+AllCells                    = unique(NucleiSegmented);
+TouchingCells               = union(unique(NucleiSegmented_MIP([1:3 end-3:end],:)),unique(NucleiSegmented_MIP(:,[1:3 end-3:end])));
+NucleiSegmented_NotBorder   = ismember(NucleiSegmented_MIP,setdiff(AllCells,TouchingCells));
+NucleiSegmented_Border      = ismember(NucleiSegmented_MIP,(TouchingCells(2:end)));
 
 
 
+%% Segment Cilia and Basal Bodies based on Peaks
+% parameters used are the prominence of the peaks and Otsu threshold
+% (internal)
+BasalBodyPeaks              = segmentBasalBody(BasalBody_MIP,20);
+GreenPeaks                  = segmentGreenPeaks(Green_MIP,100); 
+GreenPeaks_L                = bwlabel(GreenPeaks);
+greenToKeep                 = unique(GreenPeaks_L.*imdilate(BasalBodyPeaks,ones(5)));
+
+%Cilia are only cilia if they are in touch with a basal body
+ciliaSegmented              = ismember(GreenPeaks_L,greenToKeep(2:end));
+
+%In case that only the basal bodies that are close to the cilia are
+%required discriminate here
+BasalBodyPeaks_L            = bwlabel(BasalBodyPeaks);
+
+  %imagesc(ciliaPeaks*3+ basalBodyPeaks.*(1-ciliaPeaks)+2*((NucleiSegmented_MIP.*(1-basalBodyPeaks).*(1-ciliaPeaks))>0))
 
 %% Determine the final Nuclei and Cilia
 % Cilia can be discarded by size or distance to nuclei
 % Nuclei can be discarded by contact with boundary
 FinalNuclei_MIP             = NucleiSegmented_MIP.*NucleiSegmented_NotBorder;
-FinalCilia_MIP              = CiliaSegmented_MIP.*CiliaToKeep;
+FinalCilia_MIP              = ciliaSegmented;
+%FinalCilia_MIP              = CiliaSegmented_MIP.*CiliaToKeep;
+
+
+% %%
+% % Perform a hysteresis thresholding for the cilia
+% [q1,q2]                 = bwlabeln(Green>greenT_H);
+% [q3,q4]                 = bwlabeln(Green>greenT_L);
+% q5                      = ismember(q3,unique(q3(q1>0)));
+% [CiliaSegmented,q6]     = bwlabeln(ismember(q3,unique(q3(q1>0))));
+% %CiliaSegmented_P        = regionprops(CiliaSegmented,squeeze(Green),'area',"MeanIntensity","MaxIntensity","MinIntensity");
+% CiliaSegmented_MIP      = max(CiliaSegmented,[],3);
+% %CiliaSegmented_MIP_P    = regionprops(CiliaSegmented_MIP,'Area','Centroid','BoundingBox','MajorAxisLength','Circularity','Orientation','Eccentricity','MinorAxisLength','Orientation');
+% 
+% 
+% %% Discard cilia that are FAR from DAPI and are SMALL
+% DistFromDAPI            = bwdist(NucleiSegmented_MIP>0);
+ DistFromDAPI_notTouching = bwdist(NucleiSegmented_NotBorder);
+% 
+% 
+% CiliaSegmented_P2       = regionprops(CiliaSegmented,repmat(DistFromDAPI,[1 1 numSlices]),'area',"MeanIntensity","MaxIntensity","MinIntensity");
+% CiliaSegmented_P3       = regionprops(CiliaSegmented,repmat(DistFromDAPI_notTouching,[1 1 numSlices]),'area',"MeanIntensity","MaxIntensity","MinIntensity");
+% CiliaSegmented_P4       = regionprops(CiliaSegmented_MIP,'area','MajorAxisLength','MinorAxisLength','Eccentricity');
+% 
+% CiliaToKeep1             = find(([CiliaSegmented_P3.MinIntensity]<distFromNucleus)&([CiliaSegmented_P3.Area]>ciliaSize));
+% CiliaToKeep2             = find(([CiliaSegmented_P4.MajorAxisLength]./[CiliaSegmented_P4.MinorAxisLength])<10);
+% CiliaToKeep             = ismember(CiliaSegmented_MIP,intersect(CiliaToKeep1,CiliaToKeep2));
+
+
+
+
 
 FinalNuclei_MIP_P           = regionprops(FinalNuclei_MIP,'Area','Centroid','BoundingBox','MajorAxisLength','Circularity','Orientation','Eccentricity','MinorAxisLength','Orientation');
 FinalCilia_MIP_P            = regionprops(FinalCilia_MIP,'Area','Centroid','BoundingBox','MajorAxisLength','Circularity','Orientation','Eccentricity','MinorAxisLength','Orientation');
 
 
 
-%% Segment Basal Body
-basalBodyPeaks              = segmentBasalBody(BasalBody_MIP);
-
-% Only consider those regions that are next to Cilia = distance less than
-% 10 pixels
-distFromCilia               = bwdist(FinalCilia_MIP>0);
-BasalBody_MIP               = max(BasalBody,[],3);
-BasalBody_ROI               = bwlabel((FinalCilia_MIP==0).*(distFromCilia<=10));
-BasalBody_ROI_P             = regionprops(BasalBody_ROI,BasalBody_MIP,'Area','MaxIntensity','MeanIntensity','MinIntensity');
-
-% Only bright pixels, would 150 be a good threshold or determine based on 
-meanROI                     = mean(BasalBody_MIP(BasalBody_ROI>0));
-stdROI                      = std (BasalBody_MIP(BasalBody_ROI>0));
-
-
-BasalBody_0                 = ((BasalBody_ROI>0).*(BasalBody_MIP>(meanROI+3*stdROI)));
-BasalBody_1                 = imclose(BasalBody_0,ones(5));
-BasalBody_1_L               = bwlabel(BasalBody_1);
-BasalBody_1_P               = regionprops(BasalBody_1_L,BasalBody_MIP,'Area','MaxIntensity','MeanIntensity','MinIntensity');
-BasalBody_2                 = (ismember(BasalBody_1_L,find([BasalBody_1_P.Area]>2)));
-BasalBody_3                 = imdilate(BasalBody_2,ones(6,6));
-BasalBody_4                 = bwlabel((FinalCilia_MIP==0).*BasalBody_3);
-BasalBody_2_P               = regionprops(BasalBody_4,BasalBody_MIP,'Area','MaxIntensity','MeanIntensity','MinIntensity','Centroid');
+% %% Segment Basal Body
+% basalBodyPeaks              = segmentBasalBody(BasalBody_MIP);
+% 
+% % Only consider those regions that are next to Cilia = distance less than
+% % 10 pixels
+% distFromCilia               = bwdist(FinalCilia_MIP>0);
+% BasalBody_MIP               = max(BasalBody,[],3);
+% BasalBody_ROI               = bwlabel((FinalCilia_MIP==0).*(distFromCilia<=10));
+% BasalBody_ROI_P             = regionprops(BasalBody_ROI,BasalBody_MIP,'Area','MaxIntensity','MeanIntensity','MinIntensity');
+% 
+% % Only bright pixels, would 150 be a good threshold or determine based on 
+% meanROI                     = mean(BasalBody_MIP(BasalBody_ROI>0));
+% stdROI                      = std (BasalBody_MIP(BasalBody_ROI>0));
+% 
+% 
+% BasalBody_0                 = ((BasalBody_ROI>0).*(BasalBody_MIP>(meanROI+3*stdROI)));
+% BasalBody_1                 = imclose(BasalBody_0,ones(5));
+% BasalBody_1_L               = bwlabel(BasalBody_1);
+% BasalBody_1_P               = regionprops(BasalBody_1_L,BasalBody_MIP,'Area','MaxIntensity','MeanIntensity','MinIntensity');
+% BasalBody_2                 = (ismember(BasalBody_1_L,find([BasalBody_1_P.Area]>2)));
+% BasalBody_3                 = imdilate(BasalBody_2,ones(6,6));
+% BasalBody_4                 = bwlabel((FinalCilia_MIP==0).*BasalBody_3);
+% BasalBody_2_P               = regionprops(BasalBody_4,BasalBody_MIP,'Area','MaxIntensity','MeanIntensity','MinIntensity','Centroid');
 
 % 
 
@@ -158,6 +181,27 @@ BasalBody_2_P               = regionprops(BasalBody_4,BasalBody_MIP,'Area','MaxI
 % 
 % imagesc((CiliaSegmented_MIP>0)+(BasalBody_MIP<150).*(distFromCilia<10));colorbar
 
+% Distances from nuclei
+q1                          = (DistFromDAPI_notTouching.*BasalBodyPeaks);
+q1(q1==0)                   = [];
+dist_BB_Nuclei              = mean(q1);
+q1                          = (DistFromDAPI_notTouching.*FinalCilia_MIP);
+q1(q1==0)                   = [];
+dist_C_Nuclei               = mean(q1);
+
+%regions per nuclei 
+
+nucleiRegions               = watershed(DistFromDAPI_notTouching);
+
+BB_inRegions                = regionprops(BasalBodyPeaks_L,nucleiRegions,'area','MeanIntensity');
+
+BB_inRegionsDiff            = diff(sort([BB_inRegions.MeanIntensity]));
+BB_perCell                  = [sum(BB_inRegionsDiff==0) sum(BB_inRegionsDiff==1) sum(BB_inRegionsDiff>1)];
+
+skelCilia                   = bwmorph(Output.FinalCilia_MIP,'thin','inf');
+branchCilia                 = bwmorph(skelCilia,'branchpoints');
+imagesc(Output.FinalCilia_MIP+skelCilia)
+
 
 %% Prepare output
 
@@ -167,18 +211,23 @@ Output.Input_MIP_RGB(:,:,3) = DAPI_MIP;
 
 Output.FinalNuclei_MIP      = FinalNuclei_MIP;
 Output.FinalCilia_MIP       = FinalCilia_MIP;
-Output.FinalBasalBody_MIP   = BasalBody_4;
+Output.FinalBasalBody_MIP   = BasalBodyPeaks;
+
+
+ BasalBody_2_P              = regionprops(bwlabel(BasalBodyPeaks),BasalBody_MIP,'Area','MaxIntensity','MeanIntensity','MinIntensity','Centroid');
 
 Output.FinalNuclei_MIP_P    = FinalNuclei_MIP_P;
 Output.FinalCilia_MIP_P     = FinalCilia_MIP_P;
 Output.FinalBasalBody_MIP_P = BasalBody_2_P;
 Output.CiliaLength          = [Output.FinalCilia_MIP_P.MajorAxisLength]/ calibrationFactor;
 Output.NucleiLength         = [Output.FinalNuclei_MIP_P.MajorAxisLength]/ calibrationFactor;
+Output.Dist_BB_Nuclei       = dist_BB_Nuclei / calibrationFactor;
+Output.Dist_C_Nuclei        = dist_C_Nuclei / calibrationFactor;
 
 
 %% output for display, for visualisation cilia and basal body are dilated
 
-BasalBody_3                 = imdilate(BasalBody_4,ones(5));
+BasalBody_3                 = imdilate(BasalBodyPeaks,ones(5));
 FinalCilia_MIP_Dil          = imdilate(FinalCilia_MIP,ones(5));
 
 Output.FinalCombination     = 2*(BasalBody_3==0).*(FinalCilia_MIP_Dil>0) + (BasalBody_3==0).*(FinalCilia_MIP_Dil==0).*(Output.FinalNuclei_MIP>0) +3*(BasalBody_3>0);
@@ -197,6 +246,7 @@ Output.TotalBasal           = sum([Output.FinalBasalBody_MIP_P.Area]>0);
 
 Output.Ratio_C_N            = Output.TotalCilia/Output.TotalNuclei;
 Output.Ratio_B_C            = Output.TotalBasal/Output.TotalCilia;
+Output.BB_perCell           = BB_perCell/Output.TotalNuclei;
 
 Output.BasalBody_MIP        = BasalBody_MIP;
 Output.DAPI_MIP             = DAPI_MIP;        
