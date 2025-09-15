412995744f8a7b6b7d38a32d7fe98e3a5331767c
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2025-09-15 17:47:40 +0100

    Create segmentGreenPeaks.m

---
+++ CODE/segmentGreenPeaks.m
@@ -0,0 +1,23 @@
+function greenPeaks = segmentGreenPeaks(Green_MIP,prom)
+
+% Detect the cilia peaks based on findpeaks in 1D,
+
+% First start with filtering the data
+cilia_MIP_LPF                   = imfilter(Green_MIP,fspecial('gaussian',5,3),'replicate');
+greenPeaks_0                    = zeros(size(cilia_MIP_LPF));
+% run over all the rows and detect peaks, use a very high prominence to
+% only capture significant peaks
+for k=1:size(cilia_MIP_LPF,2)
+    [~,locPeaks]                = findpeaks(cilia_MIP_LPF(k,:),'MinPeakProminence',prom,'MinPeakDistance',6);
+    greenPeaks_0(k,locPeaks)    = 1;
+end
+% now extend to the regions around those peaks, use Otsu
+thres_background                =  max(cilia_MIP_LPF(:))*graythresh(cilia_MIP_LPF/max(cilia_MIP_LPF(:)));
+greenPeaks_1                    = bwlabel(cilia_MIP_LPF>(thres_background));
+greenPeaks_1P                   = regionprops(greenPeaks_1,cilia_MIP_LPF,'area','MeanIntensity','MaxIntensity');
+%keep regions that touch the peaks
+ciliaToKeep                     = unique(greenPeaks_1(greenPeaks_0>0));
+greenPeaks1                     = ismember(greenPeaks_1,ciliaToKeep(2:end));
+%discard regions that are too large (blobs)
+greenPeaks2                     = ismember(greenPeaks_1,find([greenPeaks_1P.Area]<( mean([greenPeaks_1P.Area])+ 3*std([greenPeaks_1P.Area]))));
+greenPeaks                      = greenPeaks2.*greenPeaks1;
