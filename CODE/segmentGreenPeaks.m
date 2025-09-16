7c7d2799c8c1c6c070b8f54336b0810e0e970d48
Constantino Carlos Reyes-Aldasoro <35769703+reyesaldasoro@users.noreply.github.com>
2025-09-16 13:22:44 +0100

    avoid cases of background taken as cilia

--- CODE/segmentGreenPeaks.m
+++ CODE/segmentGreenPeaks.m
@@ -1,7 +1,7 @@
 function greenPeaks = segmentGreenPeaks(Green_MIP,prom)
 
 % Detect the cilia peaks based on findpeaks in 1D,
-
+[rows,cols ] = size(Green_MIP);
 % First start with filtering the data
 cilia_MIP_LPF                   = imfilter(Green_MIP,fspecial('gaussian',5,3),'replicate');
 greenPeaks_0                    = zeros(size(cilia_MIP_LPF));
@@ -13,7 +13,11 @@ for k=1:size(cilia_MIP_LPF,2)
 end
 % now extend to the regions around those peaks, use Otsu
 thres_background                =  max(cilia_MIP_LPF(:))*graythresh(cilia_MIP_LPF/max(cilia_MIP_LPF(:)));
-greenPeaks_1                    = bwlabel(cilia_MIP_LPF>(thres_background));
+if (sum(cilia_MIP_LPF(:)>(thres_background))/rows/cols)>0.05
+    greenPeaks_1                    = bwlabel(cilia_MIP_LPF>(3*thres_background));
+else
+    greenPeaks_1                    = bwlabel(cilia_MIP_LPF>(thres_background));
+end
 greenPeaks_1P                   = regionprops(greenPeaks_1,cilia_MIP_LPF,'area','MeanIntensity','MaxIntensity');
 %keep regions that touch the peaks
 ciliaToKeep                     = unique(greenPeaks_1(greenPeaks_0>0));
